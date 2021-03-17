f1f5d332cf49ca2eef804d4e77ab27ca6e014907
thanhgis <thanh.nv@me.com>
2021-03-17 10:58:56 +0700

    Update derive_SM.py

--- pysmm/derive_SM.py
+++ pysmm/derive_SM.py
@@ -4,7 +4,7 @@ import os
 import math
 import datetime as dt
 
-def get_map(minlon, minlat, maxlon, maxlat, outpath, sampling=50, year=None, month=None, day=None, tracknr=None, overwrite=False, start=None, stop=None):
+def get_map(minlon, minlat, maxlon, maxlat, outpath, sampling=30, year=None, month=None, day=None, tracknr=None, overwrite=False, start=None, stop=None):
 
 	if year is not None:
 		# initialise GEE retrieval dataset
@@ -43,3 +43,46 @@ def get_map(minlon, minlat, maxlon, maxlat, outpath, sampling=50, year=None, mon
 		if GEE_interface.ESTIMATED_SM is not None:		
 			GEE_interface.GEE_2_asset(name=outname, timeout=False)
 			GEE_interface = None
+
+def get_ts(loc, workpath, tracknr=None, footprint=50, masksnow=True, calc_anomalies=False, create_plots=False, names=None):
+	print (loc)
+	if isinstance(loc, list):
+		print('list')
+	else:
+		loc = [loc]
+
+	if names is not None:
+		if isinstance(names, list):	print('Name list specified')
+		else:	names = [names]
+
+	sm_ts_out = list()
+	names_out = list()
+
+	cntr = 0
+	lon = loc[0]
+	lat = loc[1]
+
+	if names is not None:
+		iname = names[cntr]
+	else:
+		iname = None
+
+	print('Estimating surface soil moisture for lon: ' + str(lon) + ' lat: ' + str(lat))
+
+	# initialize GEE point object
+	gee_pt_obj = GEE_pt(lon, lat, workpath, buffer=footprint)
+	sm_ts = gee_pt_obj.extr_SM(tracknr=None, masksnow=False, tempfilter=False, calc_anomalies=calc_anomalies)
+	if iname is not None:
+		# create plots
+		if create_plots == True:	
+			if calc_anomalies == False:		
+				sm_ts_out.append(0)
+		names_out.append(iname)
+		gee_pt_obj.S1TS['117'].sort_index().to_csv(workpath + iname + '.csv')
+
+	gee_pt_obj = None
+	cntr = cntr + 1
+	if names is not None:
+		return (sm_ts_out, names_out)
+	else:
+		return sm_ts_out
