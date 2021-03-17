aac7c204c52b716f193f56f7b67c81d06639376f
thanhgis <thanh.nv@me.com>
2021-03-17 15:37:06 +0700

    Update GEE_wrappers.py

--- pysmm/GEE_wrappers.py
+++ pysmm/GEE_wrappers.py
@@ -5,7 +5,7 @@ import numpy as np
 import datetime as dt
 import math
 import time
-import os
+import os, pickle
 
 class GEE_extent(object):
 	def __init__(self, minlon, minlat, maxlon, maxlat, workdir, sampling=20):
@@ -348,7 +348,7 @@ class GEE_extent(object):
 			self.GEE_2_asset(raster=mean_gvv_v, name=mean_asset_path, timeout=False)
 			mean_gvv_v = ee.Image(self.asset_ID + mean_asset_path)
 		
-		self.check_layer(self.asset_ID + mean_asset_path)
+		pickle.dump('s1_mean', self.asset_ID + mean_asset_path)
 
 		# export
 		# self.S1_SIG0_VV_db = s1_sig0_vv
@@ -744,7 +744,7 @@ class GEE_extent(object):
 			self.GEE_2_asset(raster=gee_l8_mean, name=mean_asset_path, timeout=False)
 			gee_l8_mean = ee.Image(self.asset_ID + mean_asset_path)
 			
-		self.check_layer(self.asset_ID + mean_asset_path)
+		pickle.dump('l8_mean', self.asset_ID + mean_asset_path)
 # 		vis_params = {'min': 200, 'max': 4000}
 # 		name = 'Landsat-8'
 # 		self.mapping(gee_l8_mean, vis_params, name)
@@ -818,7 +818,8 @@ class GEE_extent(object):
 			# export asset
 			self.GEE_2_asset(raster=evi_mean, name=mean_asset_path, timeout=False)
 			evi_mean = ee.Image(self.asset_ID + mean_asset_path)
-		self.check_layer(self.asset_ID + mean_asset_path)
+		
+		pickle.dump('evi_mean', self.asset_ID + mean_asset_path)
 # 		vis_params = {'min': -650, 'max': 5000}
 # 		name = 'EVI_MODIS_Mean'
 # 		self.mapping(evi_mean, vis_params, name)
